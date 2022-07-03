df33cc801cb63e4dd9a86dcb691fc765e723e314
Honggyu08 <6398lhg@gmail.com>
2022-07-03 17:12:24 +0900

    Processing Element modified

--- PE.v
+++ PE.v
@@ -1,9 +1,13 @@
-module PE(clk, inp, add, outp);
+module PE(clk, md, inp, add, pass_d, pass_r);
 
 input clk;
 input inp;
 input add;
+input md;  // mode
 
-output outp;
+output pass_d;  // pass the data to down
+output pass_r;  // pass the data to right
 
+always@(posedge clk) begin
+    if(md) begin
 
